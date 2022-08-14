c1b61a115daf0e686fb84591c67bb27ae42ed6f4
Arbee4ever <arbee@arbeeco.de>
2022-08-14 13:01:13 +0200

    first commit

---
+++ src/main/java/de/arbeeco/minecalc/config/Config.java
@@ -0,0 +1,8 @@
+package de.arbeeco.minecalc.config;
+
+import blue.endless.jankson.Comment;
+
+public class Config {
+	@Comment("Whether the Calculator should be shown.")
+	public boolean showCalculator = true;
+}
