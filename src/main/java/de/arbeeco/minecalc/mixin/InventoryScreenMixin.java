dc8bac340fe32baf2116984088b8c6d21262f095
Arbee4ever <arbee@arbeeco.de>
2023-06-11 21:52:47 +0200

    - update to 1.20

--- src/main/java/de/arbeeco/minecalc/mixin/InventoryScreenMixin.java
+++ src/main/java/de/arbeeco/minecalc/mixin/InventoryScreenMixin.java
@@ -9,13 +9,14 @@ import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InventoryScreen.class)
 public abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> {
-	private static final Identifier CALCULATOR_BUTTON_TEXTURE = new Identifier("minecalc", "textures/gui/calculator_button.png");
+	@Unique private static final Identifier MINECALC_CALCULATOR_BUTTON_TEXTURE = new Identifier("minecalc", "textures/gui/calculator_button.png");
 	private TexturedButtonWidget button;
 
 	public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
@@ -23,15 +24,15 @@ public abstract class InventoryScreenMixin extends AbstractInventoryScreen<Playe
 	}
 
 	@Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/InventoryScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;"))
-	public void init(CallbackInfo ci) {
-		button = new TexturedButtonWidget(this.x + 129, this.height / 2 - 22, 20, 18, 0, 0, 19, CALCULATOR_BUTTON_TEXTURE, (button) -> {
+	private void addMinecalcButton(CallbackInfo ci) {
+		button = new TexturedButtonWidget(this.x + 129, this.height / 2 - 22, 20, 18, 0, 0, 19, MINECALC_CALCULATOR_BUTTON_TEXTURE, (button) -> {
 			MinecalcClient.config.showCalculator = !MinecalcClient.config.showCalculator;
 		});
 		addDrawableChild(button);
 	}
 
 	@Inject(method = "method_19891", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;setPosition(II)V"))
-	public void addDrawableChild(CallbackInfo ci) {
+	private void moveRecipeBookButton(CallbackInfo ci) {
 		button.setPosition(this.x + 129, this.height / 2 - 22);
 	}
 }
